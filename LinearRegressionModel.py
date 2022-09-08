172f53a2da9d24c48f1a757bbef7792d81034943
Warhead980 <souvikbiswas980@gmail.com>
2022-09-09 03:04:32 +0530

    OLS, WLS, GLS models for reg

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -2,19 +2,39 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import pandas as pd
+import statsmodels.api as sm
 
 
 def LinReg(df):
+    x = df[['income', 'age', 'sex']]
+    # x=x1[x1["income"] < 120000]
+    # x = np.reshape(x, (-1, 1))
 
-    df=df[df["income"] < 120000]
-    x = df['income'].tolist()
-    x = np.reshape(x, (-1, 1))
-
-    y = df['total_days'].tolist()
+    y = df['total_days']
 
     # print(x)
     # print(y)
 
+    reg = LinearRegression()
+    reg.fit(x, y)
+
+    print('Intercept: \n', reg.intercept_)
+    print('Coefficients: \n', reg.coef_)
+
+    # with statsmodels
+    x = sm.add_constant(x)  # adding a constant
+
+    modelOLS = sm.OLS(y, x).fit()
+    modelWLS = sm.WLS(y, x).fit()
+    modelGLS = sm.GLS(y, x).fit()
+    predictions = modelOLS.predict(x)
+
+    print(modelOLS.summary())
+    print(modelWLS.summary())
+    print(modelGLS.summary())
+
+
+    '''
     model = LinearRegression()
     model.fit(x, y)
     r_sq = model.score(x, y)
@@ -29,3 +49,4 @@ def LinReg(df):
     print(f"slope: {model.coef_}")
 
     y_pred = model.intercept_ + model.coef_ * x
+    '''
